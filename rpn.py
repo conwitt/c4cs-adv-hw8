228d03388384d5320ecfa85428069c24403ae068
Connor Witt <conwitt@umich.edu>
2017-10-27 11:51:06 -0400

    initial commit

---
+++ rpn.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Reverse polish notation calculator
+"""
+
+def calculate(myarg):
+    """
+    Calculate expression described in `myarg`
+    """
+    pass
+
+def main():
+    """
+    Main execution loop
+    """
+    while True:
+        calculate(input('rpn calc> '))
+
+if __name__ == '__main__':
+    main()
