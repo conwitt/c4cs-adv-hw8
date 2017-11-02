fb6255c8c47debf832248864c949547072967831
Connor Witt <conwitt@umich.edu>
2017-11-01 22:23:25 -0400

    Advanced hw part3

--- rpn.py
+++ rpn.py
@@ -4,38 +4,85 @@ Reverse polish notation calculator
 c4cs-f17-rpn
 """
 import operator
+import readline
+from termcolor import colored
 
-ops = {
+OPS = {
     '+': operator.add,
     '-': operator.sub,
     '*': operator.mul,
     '/': operator.truediv,
 }
 
-def calculate(myarg):
+def colorize(token):
     """
-    Calculate expression described in `myarg`
+    Color operators yellow, positive nums blue, and negative nums red.
     """
-    stack = []
-    tokenized = myarg.split()
-    for token in tokenized:
-        try:
-            stack.append(int(token))
-        except ValueError:
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            function = ops[token]
-            result = function(arg1, arg2)
-            stack.append(result)
-    print(stack)
-    return stack.pop()
+    if token in OPS:
+        return colored(token, 'yellow')
+    elif isinstance(token, (int, float)):
+        if token >= 0:
+            return colored(token, 'blue')
+        return colored(token, 'red')
+    return colored(token, 'green')
+
+def cast_whole_float(num):
+    """
+    Casts a float to an int if whole number
+    """
+    if isinstance(num, float):
+        return int(num) if num.is_integer() else num
+    return num
+
+class Calculator(object):
+    """
+    Reverse polish notation calculator
+    """
+    def __init__(self):
+        self.stack = []
+        self.log_stack = []
+        self.last_result = 0
+
+    def calculate(self, in_calc):
+        """
+        Calculate expression described in `in_calc`
+        """
+        tokenized = in_calc.split()
+        for token in tokenized:
+            try:
+                self.stack.append(int(token))
+            except ValueError:
+                if token in OPS and len(self.stack) >= 2:
+                    arg2 = self.stack.pop()
+                    arg1 = self.stack.pop()
+                    function = OPS[token]
+                    result = cast_whole_float(function(arg1, arg2))
+                    self.stack.append(result)
+                    self.last_result = result
+
+                    self.log_stack.append([
+                        colorize(token) for token in [arg1, arg2, token, result]
+                    ])
+                    print(colorize(self.last_result))
+                else:
+                    self.print_log()
+
+        return self.last_result
+
+    def print_log(self):
+        """
+        prints the log stack
+        """
+        for equation in self.log_stack:
+            print(' '.join(equation))
 
 def main():
     """
     Main execution loop
     """
+    calc = Calculator()
     while True:
-        calculate(input('rpn calc> '))
+        calc.calculate(input('rpn calc> '))
 
 if __name__ == '__main__':
     main()
--- test_rpn.py
+++ test_rpn.py
@@ -3,36 +3,39 @@ Tests for rpn.py
 """
 
 import unittest
-import rpn
+from rpn import Calculator
 
 class TestBasics(unittest.TestCase):
     """
     Basic tests for rpn.y
     """
+    def setUp(self):
+        self.calc = Calculator()
+
     def test_add(self):
         """
         Test for addition
         """
-        result = rpn.calculate('1 1 +')
+        result = self.calc.calculate('1 1 +')
         self.assertEqual(2, result)
 
     def test_subtract(self):
         """
         Test for subtraction
         """
-        result = rpn.calculate('5 3 -')
+        result = self.calc.calculate('5 3 -')
         self.assertEqual(2, result)
 
     def test_multiply(self):
         """
         Test for multiplication
         """
-        result = rpn.calculate('5 6 *')
+        result = self.calc.calculate('5 6 *')
         self.assertEqual(30, result)
 
     def test_divide(self):
         """
         Test for division
         """
-        result = rpn.calculate('30 6 /')
+        result = self.calc.calculate('30 6 /')
         self.assertEqual(5, result)
